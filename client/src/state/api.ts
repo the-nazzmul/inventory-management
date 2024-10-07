75072e32bee6ed1d77239d571c96c355e6a83c68
the-nazzmul <nazmul.hussain.utchash@gmail.com>
2024-10-07 11:15:24 +0600

    added dashboard home page layout with grid

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,10 +1,16 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { DashboardMetrics } from "@/utils/interfaces";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
   reducerPath: "api",
-  tagTypes: [],
-  endpoints: (build) => ({}),
+  tagTypes: ["DashboardMetrics"],
+  endpoints: (build) => ({
+    getDashboardMetrics: build.query<DashboardMetrics, void>({
+      query: () => "/dashboard",
+      providesTags: ["DashboardMetrics"],
+    }),
+  }),
 });
 
-export const {} = api;
+export const { useGetDashboardMetricsQuery } = api;
