54c498982b8a51bb7278a106ed9f9836fc058621
the-nazzmul <nazmul.hussain.utchash@gmail.com>
2024-10-08 14:22:01 +0600

    added expense summary but not complete

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import ExpenseSummary from "@/components/dashboard/cards/ExpenseSummaryCard";
 import PopularProductsCard from "@/components/dashboard/cards/PopularProductsCard";
 import PurchaseSummaryCard from "@/components/dashboard/cards/PurchaseSummaryCard";
 import SalesSummaryCard from "@/components/dashboard/cards/SalesSummaryCard";
@@ -10,8 +11,7 @@ const Dashboard = () => {
       <PopularProductsCard />
       <SalesSummaryCard />
       <PurchaseSummaryCard />
-      <div className="row-span-2 xl:row-span-3 col-span-1 md:col-span-2 xl:col-span-1 bg-gray-500"></div>
-      <div className="row-span-3 bg-gray-500"></div>
+      <ExpenseSummary />
       <div className="md:row-span-1 xl:row-span-2 bg-gray-500"></div>
       <div className="md:row-span-1 xl:row-span-2 bg-gray-500"></div>
       <div className="md:row-span-1 xl:row-span-2 bg-gray-500"></div>
---
+++ client/src/components/dashboard/cards/ExpenseSummaryCard.tsx
@@ -0,0 +1,93 @@
+import { useGetDashboardMetricsQuery } from "@/state/api";
+import { ExpenseByCategorySummary } from "@/utils/interfaces";
+import { Cell, Pie, PieChart, ResponsiveContainer } from "recharts";
+
+type TExpenseSum = {
+  [category: string]: number;
+};
+
+const colors = ["#00C49F", "#0088FE", "#FFBB28"];
+
+const ExpenseSummary = () => {
+  const { data: dashboardMetrics, isLoading } = useGetDashboardMetricsQuery();
+  console.log(dashboardMetrics);
+
+  const expenseByCategorySummary =
+    dashboardMetrics?.expenseByCategorySummary || [];
+
+  const expenseSums = expenseByCategorySummary.reduce(
+    (acc: TExpenseSum, item: ExpenseByCategorySummary) => {
+      const category = item.category + " Expenses";
+      const amount = parseInt(item.amount, 10);
+      if (!acc[category]) acc[category] = 0;
+      acc[category] += amount;
+      return acc;
+    },
+    {}
+  );
+
+  const expenseCategories = Object.entries(expenseSums).map(
+    ([name, value]) => ({
+      name,
+      value,
+    })
+  );
+
+  const formattedTotalExpenses = expenseCategories
+    .reduce((acc, category: { value: number }) => acc + category.value, 0)
+    .toFixed(2);
+
+  return (
+    <div className="row-span-3 bg-white shadow-md rounded-2xl flex flex-col justify-between">
+      {isLoading ? (
+        <div className="m-5">Loading...</div>
+      ) : (
+        <>
+          {/* HEADER */}
+          <div>
+            <h2 className="text-lg font-semibold mb-2 px-7 pt-5">
+              Expense Summary
+            </h2>
+            <hr />
+          </div>
+          {/* BODY */}
+          <div className="xl:flex justify-between pr-7">
+            {/* CHART */}
+            <div className="relative basis-3/5">
+              <ResponsiveContainer width="100%" height={140}>
+                <PieChart>
+                  <Pie
+                    data={expenseCategories}
+                    innerRadius={50}
+                    outerRadius={60}
+                    fill="#8884d8"
+                    dataKey="value"
+                    nameKey="name"
+                    cx="50%"
+                    cy="50%"
+                  >
+                    {expenseCategories.map((entry, index) => (
+                      <Cell
+                        key={`cell-${index}`}
+                        fill={colors[index % colors.length]}
+                      />
+                    ))}
+                  </Pie>
+                </PieChart>
+              </ResponsiveContainer>
+              <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 basis-2/5">
+                <span className="font-bold text-xl">
+                  ${formattedTotalExpenses}
+                </span>
+              </div>
+            </div>
+            {/* LABELS */}
+            
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default ExpenseSummary;
